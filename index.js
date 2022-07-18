9e01e136066d190ecb9c92ff22e7dcbead070aa4
Sashin Agrawal <sashin_agrawal@epam.com>
2022-07-18 11:56:44 +0530

    sql file

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-import express from 'express';
-import { graphqlHTTP }  from 'express-graphql';
-import { schema } from './data/schema';
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import { schema } from "./data/schema";
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Graphql is amazing!');
+app.get("/", (req, res) => {
+	res.send("Graphql is amazing!");
 });
 
-app.use('/graphql', graphqlHTTP({
-    schema: schema,
-    graphiql: true,
-}));
+app.use(
+	"/graphql",
+	graphqlHTTP({
+		schema: schema,
+		graphiql: true,
+	})
+);
 
-app.listen(8080, () => console.log('Running on server port localhost:8080/graphql'));
+const helmet = require("helmet");
+app.use(helmet.frameguard({ action: "DENY" }));
+
+app.listen(8082, () =>
+	console.log("Running on server port localhost:8082/graphql")
+);
